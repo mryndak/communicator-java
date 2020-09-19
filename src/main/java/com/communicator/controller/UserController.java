d4c4da147306e46cf77936dc2639a22f58bcca62
QueIvan <szewczykjan2@gmail.com>
2020-09-19 22:42:36 +0200

    Localhost changes

--- src/main/java/com/communicator/controller/UserController.java
+++ src/main/java/com/communicator/controller/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
-//@CrossOrigin(origins = {"${settings.cross_origin}"})
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/v1/users")
 @RequiredArgsConstructor
