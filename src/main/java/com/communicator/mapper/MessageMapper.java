b0f118116134a0232f99df8e37d401ac0d6a3f5c
QueIvan <szewczykjan2@gmail.com>
2020-09-29 14:33:24 +0200

    Corrections

--- src/main/java/com/communicator/mapper/MessageMapper.java
+++ src/main/java/com/communicator/mapper/MessageMapper.java
@@ -3,7 +3,6 @@ package com.communicator.mapper;
 import com.communicator.domain.Message;
 import com.communicator.domain.MessageBlopDto;
 import com.communicator.domain.MessageDto;
-import com.communicator.domain.MessageReceivedDto;
 import org.mapstruct.Mapper;
 import org.mapstruct.ReportingPolicy;
 
@@ -12,17 +11,9 @@ import java.util.List;
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface MessageMapper {
     Message mapToMessage(MessageBlopDto messageBlopDto);
-    Message mapToMessage(MessageReceivedDto messageReceivedDto);
     Message mapToMessage(MessageDto messageDto);
     MessageBlopDto mapToMessageBlopDto(Message message);
-    MessageBlopDto mapToMessageBlopDto(MessageReceivedDto messageReceivedDto);
-    MessageBlopDto mapToMessageBlopDto(MessageDto messageDto);
-    MessageReceivedDto mapToMessageReceivedDto(Message message);
-    MessageReceivedDto mapToMessageReceivedDto(MessageBlopDto messageBlopDto);
-    MessageReceivedDto mapToMessageReceivedDto(MessageDto messageDto);
     MessageDto mapToMessageDto(Message message);
-    MessageDto mapToMessageDto(MessageBlopDto messageBlopDto);
-    MessageDto mapToMessageDto(MessageReceivedDto messageReceivedDto);
     List<Message> mapMessageBlopDtoListToMessageList(List<MessageBlopDto> messageBlopDtoList);
     List<MessageBlopDto> mapMessageListToMessageBlopList(List<Message> messageBlopDtoList);
 }
--- src/main/java/com/communicator/service/MailCreatorService.java
+++ src/main/java/com/communicator/service/MailCreatorService.java
@@ -12,13 +12,12 @@ import org.thymeleaf.context.Context;
 public class MailCreatorService {
 
     private final TemplateEngine templateEngine;
-    private final UserService userService;
     private final AdminConfig adminConfig;
 
     public String buildDailyReportMail(final Mail mail){
         Context context = new Context();
         context.setVariable("userData", mail.getRecipient());
-        context.setVariable("unreadMessages", mail.getUnreadMessage());
+        context.setVariable("unreadMessages", mail.getUnreadMessage().toString());
         context.setVariable("adminConfig", adminConfig);
         return templateEngine.process("mail/daily-mail-template.html", context);
     }
